caf1a0c3d85b8ccfe86dfad125f3faa12ed593e3
mardigiov <mdigiovanni2000@gmail.com>
2024-03-10 15:21:31 -0400

    The about page is completed with all of our information and a button to send the user back to the main menu. Customization will be needed in the future.

---
+++ app/src/main/java/edu/msu/cse476/adiwidj1/cookie_plus/AboutActivity.java
@@ -0,0 +1,19 @@
+package edu.msu.cse476.adiwidj1.cookie_plus;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    public void goBackToMainMenu(View view) {
+        finish();
+    }
+}
--- app/src/main/java/edu/msu/cse476/adiwidj1/cookie_plus/MainActivity.java
+++ app/src/main/java/edu/msu/cse476/adiwidj1/cookie_plus/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.msu.cse476.adiwidj1.cookie_plus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,12 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button quitButton = findViewById(R.id.buttonQuit);
-        quitButton.setOnClickListener(new View.OnClickListener() {
+        Button aboutButton = findViewById(R.id.buttonAbout);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                openAboutPage();
             }
         });
+    }
+
+    public void openAboutPage() {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 }
